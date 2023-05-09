29caa3efd8ff879ea028d3e50cd98a196810bf6d
rabayarabu <rabu9149@gmail.com>
2023-05-09 22:03:33 +0600

    Include linter configurations

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import './style.css';
 
 const todos = [
-    {
-        index: 1,
-        completed: false,
-        description: "eat rice"
-    
-    },
-    {
-        index: 2,
-        completed: false,
-        description: "walking outside"
-    
-    },
-    {
-        index: 3,
-        completed: false,
-        description: "taking shower"
-    
-    }
+  {
+    index: 1,
+    completed: false,
+    description: 'eat rice',
+
+  },
+  {
+    index: 2,
+    completed: false,
+    description: 'walking outside',
+
+  },
+  {
+    index: 3,
+    completed: false,
+    description: 'taking shower',
+
+  },
 ];
+
+export default todos;
