39dd3652a73c193144e5c7a03fd56f12d5a9c1a3
rabayarabu <rabu9149@gmail.com>
2023-05-09 23:32:43 +0600

    To do list added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import './style.css';
 
-const todos = [
-  {
-    index: 1,
-    completed: false,
-    description: 'eat rice',
+import addTodo from '../modules/addTodo.js';
+import TodoList from '../modules/workClass.js';
 
-  },
-  {
-    index: 2,
-    completed: false,
-    description: 'walking outside',
-
-  },
-  {
-    index: 3,
-    completed: false,
-    description: 'taking shower',
-
-  },
-];
-
-export default todos;
+const todoclass = new TodoList();
+todoclass.workList();
+addTodo();
