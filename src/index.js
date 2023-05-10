5bac73f8e49b13ffab9d61a693db5ec026bed9cd
rabayarabu <rabu9149@gmail.com>
2023-05-10 12:19:59 +0600

    Fix required changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import './style.css';
 
-import addTodo from '../modules/addTodo.js';
-import TodoList from '../modules/workClass.js';
+const todos = [{
+  index: 1,
+  description: 'Doctor appointment',
+  completed: false,
+},
+{
+  index: 2,
+  description: 'Exam preparartion',
+  completed: false,
+},
+{
+  index: 3,
+  description: 'Ellectric bill pay',
+  completed: false,
+},
+{
+  index: 4,
+  description: 'Shopping',
+  completed: false,
+},
+];
+// const todoclass = new TodoList();
+// todoclass.workList();
+// addTodo();
 
-const todoclass = new TodoList();
-todoclass.workList();
-addTodo();
+const workList = () => {
+  const worklist = document.querySelector('.to-do-list');
+  const deleteBtn = document.getElementById('delete');
+
+  deleteBtn.style.display = 'none';
+
+  if (todos.length > 0) {
+    worklist.innerHTML = todos.map((todo) => `<li class="each-todo"><input type="checkbox"> <p>${todo.description}</p><i class="fa-solid fa-trash"></i></li>`).join('');
+    deleteBtn.style.display = 'block';
+  }
+};
+
+workList();
